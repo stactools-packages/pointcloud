516cf31aa6547152a8f2bdcab1fab44c700e8c1b
Thomas Maschler <thomas.maschler@planet.com>
2023-08-25 13:31:57 -0400

    fix unit tests

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,22 +1,25 @@
 import os
+import unittest
+
 from tempfile import TemporaryDirectory
 
 import pystac
 
+from click import Group
+from click.testing import CliRunner
 from stactools.pointcloud.commands import create_pointcloud_command
-from tests.utils import (TestData, CliTestCase)
 
 
-class CreateItemTest(CliTestCase):
-
-    def create_subcommand_functions(self):
-        return [create_pointcloud_command]
+class CreateItemTest(unittest.TestCase):
 
     def test_create_item(self):
-        href = TestData.get_path('data-files/pointcloud/autzen_trim.las')
+        href = f"{os.path.dirname(__file__)}/data-files/autzen_trim.las"
         with TemporaryDirectory() as directory:
-            cmd = ['pointcloud', 'create-item', href, directory]
-            self.run_command(cmd)
+            runner = CliRunner()
+            runner.invoke(
+                create_pointcloud_command(Group()),
+                ["create-item", href, directory],
+            )
             jsons = [p for p in os.listdir(directory) if p.endswith('.json')]
             self.assertEqual(len(jsons), 1)
             item_path = os.path.join(directory, jsons[0])
--- tests/test_stac.py
+++ tests/test_stac.py
@@ -1,34 +1,37 @@
 import datetime
+import os
 import unittest
 
-from tests.utils import TestData
+from pystac.extensions.pointcloud import PointcloudExtension
+from pystac.extensions.projection import ProjectionExtension
 from stactools.pointcloud.stac import create_item
 
 
 class StacTest(unittest.TestCase):
 
     def test_create_item(self):
-        path = TestData.get_path("data-files/pointcloud/autzen_trim.las")
+        path = f"{os.path.dirname(__file__)}/data-files/autzen_trim.las"
         item = create_item(path)
         self.assertEqual(item.id, "autzen_trim")
         self.assertEqual(item.datetime, datetime.datetime(2015, 9, 10))
 
-        item.ext.enable("projection")
-        self.assertEqual(item.ext.projection.epsg, 2994)
+        projection_ext = ProjectionExtension.ext(item, add_if_missing=True)
+        self.assertEqual(projection_ext.epsg, 2994)
 
-        item.ext.enable("pointcloud")
-        self.assertEqual(item.ext.pointcloud.count, 110000)
-        self.assertEqual(item.ext.pointcloud.type, "lidar")
-        self.assertEqual(item.ext.pointcloud.encoding, "las")
-        self.assertEqual(item.ext.pointcloud.statistics, None)
-        self.assertTrue(item.ext.pointcloud.schemas)
+        pointcloud_ext = PointcloudExtension.ext(item, add_if_missing=True)
+        self.assertEqual(pointcloud_ext.count, 110000)
+        self.assertEqual(pointcloud_ext.type, "lidar")
+        self.assertEqual(pointcloud_ext.encoding, "las")
+        self.assertEqual(pointcloud_ext.statistics, None)
+        self.assertTrue(pointcloud_ext.schemas)
 
         item.validate()
 
     def test_create_item_with_statistic(self):
-        path = TestData.get_path("data-files/pointcloud/autzen_trim.las")
+        path = f"{os.path.dirname(__file__)}/data-files/autzen_trim.las"
         item = create_item(path, compute_statistics=True)
-        self.assertNotEqual(item.ext.pointcloud.statistics, None)
+        pointcloud_ext = PointcloudExtension.ext(item, add_if_missing=True)
+        self.assertNotEqual(pointcloud_ext.statistics, None)
 
     def test_create_item_from_url(self):
         url = "https://github.com/PDAL/PDAL/raw/2.2.0/test/data/las/autzen_trim.las"
