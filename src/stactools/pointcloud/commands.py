3e312d32c4cf84b0ae2e2e894adb240127ba4c34
Laura, Jason R <jlaura@usgs.gov>
2023-09-27 06:25:56 -0700

    feat: Adds support for non-4326, lat/lon proj
    No check that the user isn't passing a garbage (non-lat/lon CRS).

--- src/stactools/pointcloud/commands.py
+++ src/stactools/pointcloud/commands.py
@@ -1,10 +1,9 @@
-import logging
 import json
+import logging
 import os
 
 import click
 import pystac
-
 from stactools.pointcloud.stac import create_item
 
 logger = logging.getLogger(__name__)
@@ -15,30 +14,41 @@ def create_pointcloud_command(cli):
     pointclouds.
     """
 
-    @cli.group('pointcloud',
+    @cli.group("pointcloud",
                short_help=("Commands for working with "
                            "pointclouds."))
     def pointcloud():
         pass
 
-    @pointcloud.command('create-item',
+    @pointcloud.command("create-item",
                         short_help="Create a STAC Item from a las or laz file")
-    @click.argument('href')
-    @click.argument('dst')
-    @click.option('-r', '--reader', help='Override the default PDAL reader.')
-    @click.option('-t',
-                  '--pointcloud-type',
-                  default="lidar",
-                  help='Set the pointcloud type (default: lidar)')
+    @click.argument("href")
+    @click.argument("dst")
+    @click.option("-r", "--reader", help="Override the default PDAL reader.")
     @click.option(
-        '--compute-statistics/--no-compute-statistics',
+        "-t",
+        "--pointcloud-type",
+        default="lidar",
+        help="Set the pointcloud type (default: lidar)",
+    )
+    @click.option(
+        "--compute-statistics/--no-compute-statistics",
         default=False,
-        help='Compute statistics for the pointcloud (could take a while)')
+        help="Compute statistics for the pointcloud (could take a while)",
+    )
     @click.option(
-        '-p',
-        '--providers',
-        help='Path to JSON file containing array of additional providers')
-    def create_item_command(href, dst, reader, pointcloud_type,
+        "-p",
+        "--providers",
+        help="Path to JSON file containing array of additional providers",
+    )
+    @click.option(
+        "-a",
+        "--a_srs",
+        help=("Projection string for reprojecting native coordinates to "
+              "create a lat/lon geom for the STAC item"),
+        default="EPSG:4326",
+    )
+    def create_item_command(href, dst, reader, pointcloud_type, a_srs,
                             compute_statistics, providers):
         """Creates a STAC Item based on the header of a pointcloud.
 
@@ -53,13 +63,16 @@ def create_pointcloud_command(cli):
                     pystac.Provider.from_dict(d) for d in json.load(f)
                 ]
 
-        item = create_item(href,
-                           pdal_reader=reader,
-                           compute_statistics=compute_statistics,
-                           pointcloud_type=pointcloud_type,
-                           additional_providers=additional_providers)
+        item = create_item(
+            href,
+            pdal_reader=reader,
+            compute_statistics=compute_statistics,
+            pointcloud_type=pointcloud_type,
+            a_srs=a_srs,
+            additional_providers=additional_providers,
+        )
 
-        item_path = os.path.join(dst, '{}.json'.format(item.id))
+        item_path = os.path.join(dst, "{}.json".format(item.id))
         item.set_self_href(item_path)
         item.validate()
         item.save_object()
